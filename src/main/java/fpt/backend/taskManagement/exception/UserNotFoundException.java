e562a6999658fdbcda63a912660b21557ebfc55f
Nguyen-Dang-Khoa-04072004 <khoa47245@gmail.com>
2025-06-23 14:13:55 +0700

    fix: remove error import

--- src/main/java/fpt/backend/taskManagement/exception/UserNotFoundException.java
+++ src/main/java/fpt/backend/taskManagement/exception/UserNotFoundException.java
@@ -1,6 +1,5 @@
 package fpt.backend.taskManagement.exception;
 
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException() {

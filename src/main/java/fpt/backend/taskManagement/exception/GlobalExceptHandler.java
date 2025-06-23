b125ec160a23f311fd40af7454cf9e6712bbf9d0
Nguyen-Dang-Khoa-04072004 <khoa47245@gmail.com>
2025-06-23 14:10:02 +0700

    fix: remove import package

--- src/main/java/fpt/backend/taskManagement/exception/GlobalExceptHandler.java
+++ src/main/java/fpt/backend/taskManagement/exception/GlobalExceptHandler.java
@@ -1,14 +1,13 @@
 package fpt.backend.taskManagement.exception;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptHandler {
 
-    @ExceptionHandler({TaskNotFoundException.class, UserNotFoundException.class, BadCredentialsException.class})
+    @ExceptionHandler({TaskNotFoundException.class, UserNotFoundException.class})
     public ResponseEntity<ExceptionResponse> handleTaskNotFoundExcept(Exception e){
         return ResponseEntity.badRequest().body(new ExceptionResponse("Failed",e.getMessage()));
     }
